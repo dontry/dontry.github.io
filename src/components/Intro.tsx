14ba48ed68c1113d00e433bd39b4907419b0b6d5
dontry <mccoy018@gmail.com>
2024-12-30 20:50:06 +1100

    add fuzzy search feature

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import SocialLinks from './SocialLinks';
 
 const Intro: React.FC = () => (
   <div className="text-center">
     <h2 className="font-medium mb-2">Dong Cai</h2>
     <h4 className="font-normal mb-4">Web Developer</h4>
-    <SocialLinks />
   </div>
 );
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,31 +2,45 @@ import React from 'react';
 import Container from './Container';
 import NavLink from './NavLink';
 import Logo from './Logo';
+import Search from './Search';
 
 interface NavBarProps {
   pathname?: string;
+  posts: Array<{
+    id: string;
+    data: {
+      title: string;
+      excerpt?: string;
+      date?: Date;
+      tags?: string[];
+    };
+  }>;
 }
 
-const NavBar: React.FC<NavBarProps> = ({ pathname = '' }) => {
+const NavBar: React.FC<NavBarProps> = ({ pathname = '', posts = [] }) => {
   return (
     <header className="sticky top-0 left-0 right-0 z-10 w-full bg-white text-dark-grey">
       <Container>
         <div className="flex items-center justify-between border-b px-4 py-2">
           <Logo />
-          <nav className="flex items-stretch overflow-x-auto overflow-y-hidden 
-            touch-pan-x h-full w-[60%] xs:flex-grow xs:w-auto xl:w-auto ml-10">
-            <NavLink isActive={pathname === '/'} title="Home" to="/" />
-            <NavLink
-              isActive={pathname.includes('/archive')}
-              title="Archives"
-              to="/archives"
-            />
-            <NavLink
-              isActive={pathname === '/about'}
-              title="About"
-              to="/about"
-            />
-          </nav>
+          <div className="flex items-center flex-1 justify-end gap-4">
+            {/* <SearchProvider posts={posts} /> */}
+            <Search posts={posts} />
+            <nav className="flex items-stretch overflow-x-auto overflow-y-hidden 
+              touch-pan-x h-full">
+              <NavLink isActive={pathname === '/'} title="Home" to="/" />
+              <NavLink
+                isActive={pathname.includes('/archive')}
+                title="Archives"
+                to="/archives"
+              />
+              <NavLink
+                isActive={pathname === '/about'}
+                title="About"
+                to="/about"
+              />
+            </nav>
+          </div>
         </div>
       </Container>
     </header>
